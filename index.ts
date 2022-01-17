31210576b70ddda0e85dbc0caa5a66a390fb3c82
Saad Ahmed Siddiqui <48211799+saadjhk@users.noreply.github.com>
2022-01-17 18:45:50 +0500

    Try test populate

--- index.ts
+++ index.ts
@@ -1,12 +1,22 @@
 require("dotenv").config();
+import Keyring from "@polkadot/keyring";
 import { cryptoWaitReady } from "@polkadot/util-crypto";
+import { crowdloanRewardsPopulateTest } from "./pallets/crowdloanRewards";
+import { buildApi } from "./utils";
 
 const main = async () => {
+    await cryptoWaitReady();
 
+    const walletAlice = new Keyring().addFromUri("//Alice");
+    const api = await buildApi(process.env.PICASSO_RPC_URL || "");
+    const tx = await crowdloanRewardsPopulateTest(api, walletAlice);
+    console.log(tx);
+    process.exit(0);
 };
 
 cryptoWaitReady().then(() => {
     main().catch((err) => {
-        console.log(err.message);
+        console.error(err.message);
+        process.exit(0);
     });
 });
--- utils.ts
+++ utils.ts
@@ -1,26 +1,19 @@
 import { ApiPromise, WsProvider } from "@polkadot/api";
 import { IKeyringPair } from "@polkadot/types/types";
 import Web3 from "web3";
-const web3 = new Web3(process.env.GANACHE_URL || "")
+const web3 = new Web3(process.env.GANACHE_URL || "");
 
-export const buildApi = async (substrateNodeUrl: string): Promise<ApiPromise | null> => {
-    try {
-        const wsProvider = new WsProvider(process.env.PICASSO_RPC_URL!, 1000);
-        const polkadotApi = new ApiPromise({ provider: wsProvider });
+export const buildApi = async (substrateNodeUrl: string): Promise<ApiPromise> => {
+    const wsProvider = new WsProvider(substrateNodeUrl, 1000);
+    const polkadotApi = new ApiPromise({ provider: wsProvider });
 
-        return await polkadotApi.isReady;
-    } catch (err: any) {
-        console.error(`Error Connecting: ${err.message}`);
-        return null;
-    }
+    return await polkadotApi.isReady;
 };
 
 export const toHexString = (bytes: Uint8Array) =>
     Array.prototype.map.call(bytes, (x) => ("0" + (x & 0xff).toString(16)).slice(-2)).join("");
 
-    // The prefix is defined as pallet config
-export const proofMessage = (account: IKeyringPair) =>
-  "picasso-" + toHexString(account.publicKey);
+// The prefix is defined as pallet config
+export const proofMessage = (account: IKeyringPair) => "picasso-" + toHexString(account.publicKey);
 
-export const ethAccount = (seed: number) =>
-  web3.eth.accounts.privateKeyToAccount("0x" + seed.toString(16).padStart(64, '0'))
+export const ethAccount = (seed: number) => web3.eth.accounts.privateKeyToAccount("0x" + seed.toString(16).padStart(64, "0"));
