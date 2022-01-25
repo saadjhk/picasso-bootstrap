6e5c1a79b6ae75b2092f9b126030d37e7a27b656
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-01-25 17:15:58 +0500

    Works.

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,21 @@
-import './interfaces/augment-api';
-import './interfaces/augment-types';
-
 require("dotenv").config();
 import Keyring from "@polkadot/keyring";
-import { cryptoWaitReady, signatureVerify } from "@polkadot/util-crypto";
-import { associateKSM, crowdloanRewardsPopulateTest, initialize } from "./pallets/crowdloanRewards";
-import { buildApi, toHexString } from "./utils";
+import { cryptoWaitReady } from "@polkadot/util-crypto";
+import { crowdloanRewardsPopulateTest, initialize } from "./pallets/crowdloanRewards";
+
 import * as definitions from './interfaces/definitions';
+import { buildApi } from "@/utils";
+
 const main = async () => {
     const types = Object.values(definitions).reduce((res, { types }): object => ({ ...res, ...types }), {});
-
     await cryptoWaitReady();
     const api = await buildApi(process.env.PICASSO_RPC_URL || "", types);
     const kr = new Keyring({ type: "sr25519" })
-    const walletSudo = kr.addFromUri("//Alice"); // alice
-    const populatetx = await crowdloanRewardsPopulateTest(api, walletSudo);
-    const initTx = await initialize(api, walletSudo);
-    const tx = await associateKSM(api, walletSudo.derive('/contributor-40'), walletSudo.derive('/reward-40'));
 
-    console.log(populatetx);
-    console.log(initTx);
-    console.log(tx)
+    const walletSudo = kr.addFromUri("//Alice"); // alice
+
+    await crowdloanRewardsPopulateTest(api, walletSudo);
+    await initialize(api, walletSudo);
     process.exit(0);
 };
 
