4e4893234c1399b17f6400bcf998cc005f983099
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-03-02 18:11:26 +0500

    fix minting for polkadot and ksm custom addresses, sending funds to pallet account included

--- src/index.ts
+++ src/index.ts
@@ -67,7 +67,7 @@ const main = async () => {
     process.env.ETH_PK ? process.env.ETH_PK : ""
   );
   const myEth2 = new ethers.Wallet(
-    process.env.ETH_PK ? process.env.ETH_PK : ""
+    process.env.ETH_PK1 ? process.env.ETH_PK1 : ""
   );
 
   const rpc = Object.keys(definitions).reduce(
@@ -141,64 +141,77 @@ const main = async () => {
   //   address: myEth1.address.toLowerCase(),
   //   rewards: "1223.231",
   // });
-
   // const crPopRes = await crowdloanRewardsPopulateJSON(api, walletSudo, ksm.slice(0, 1500), eth.slice(0,500));
 
   const palletId = "5w3oyasYQg6vkbxZKeMG8Dz2evBw1P7Xr7xhVwk4qwwFkm8u";
-  // const txM = await api.tx.sudo.sudo(api.tx.assets
-  //   .mintInto(
-  //     1,
-  //     walletSudo.publicKey,
-  //     api.createType("u128", (15000 * 1e12).toString())
-  //   ))
-  //   .signAndSend(walletSudo, async (result) => {
-  //     console.log(`Current status is ${result.status}`);
-  //     if (result.status.isInBlock) {
-  //       console.log(
-  //         `Transaction included at blockHash ${result.status.asInBlock}`
-  //       );
-  //     } else if (result.status.isFinalized) {
-  //       console.log(
-  //         `Transaction finalized at blockHash ${result.status.asFinalized}`
-  //       );
-  //     }
-  //   });
+  const txM = await api.tx.sudo
+    .sudo(
+      api.tx.assets.mintInto(
+        1,
+        walletSudo.publicKey,
+        api.createType("u128", (1400 * 1e12).toString())
+      )
+    )
+    .signAndSend(walletSudo, async (result) => {
+      console.log(`[MINT] Current status is ${result.status}`);
+      if (result.status.isInBlock) {
+        console.log(
+          `[MINT] Transaction included at blockHash ${result.status.asInBlock}`
+        );
+      } else if (result.status.isFinalized) {
+        console.log(
+          `[MINT] Transaction finalized at blockHash ${result.status.asFinalized}`
+        );
 
-  // const tx = await api.tx.tokens.transfer(palletId, 0, api.createType("u128", (15000 * 1e12).toString())).signAndSend(walletSudo, async (result1) => {
-  //   console.log(`Current status is ${result1.status}`);
-  //   if (result1.status.isInBlock) {
-  //     console.log(`Transaction included at blockHash ${result1.status.asInBlock}`);
-  //   } else if (result1.status.isFinalized) {
-  //     console.log(`Transaction finalized at blockHash ${result1.status.asFinalized}`);
-  //   }
-  // });
-  const crPopRes = await crowdloanRewardsPopulateJSON(
-    api,
-    walletSudo,
-    [
-      { address: "5tfaf3MPRwzECcLhnzv75zvML1DHGJcvPYamoSNLoeAgGQ4S", rewards: "100"},
-      { address: "5y1hoDbgnAKsL9JZ3d1zkM5LYQQPaAnb9Un7VXMM2YipSeUE", rewards: "100"},
-      // peter dot wallets
-      { address: "5uymjr2xLL14upmg4nezH5LZMNgenGn7MrbQ2WnJ7dhcDb4C", rewards: "100"},
-      { address: "5z6opGwNemAtYG7o7KehBn2KdKbGPw64E23ZpwxcXoGiwufL", rewards: "100"},
-      { address: "E4syq7LfkjrZuqofYRg5dX2zzd8DR54p82F2BHuFLqHHGm6", rewards: "100"},
-      // { address: "GRxsfLzj5wthacZ6bYSdL9FNosAMFBkVhcwWWxGtMsCSx8G", rewards: "100"},
-      // { address: "FhJDi6usuBii4kbHEiUbYcd2a1yXk5CJCJEkxr2BT3wqHmc", rewards: "100"},
-      // { address: "5GgjZECB6XsH3iao7rg6dDbMG9urjsWVjinDBF2ngqWFxyoC", rewards: "100"},
-      { address: "F53d3jeyFvb2eYsgAERhjC8mogao4Kg4GsdezrqiT8aj55v", rewards: "100"}, // liviu
-    ],
-    [
-      { address: myEth1.address, rewards: "100"},
-      { address: myEth2.address, rewards: "100"},
-      // peter eth wallets
-      { address: "0x33Cd07B1ae8485a6090091ee55389237eCB0Aed4", rewards: "100"},
-      { address: "0xfe302f2D69cAf32d71812587ECcd4fcDF8287E22", rewards: "100"},
-      { address: "0x38650E1FD89E6bBEfDD2f150190C70da02454b93", rewards: "100"},
-    ]
-  );
+        const tx = await api.tx.assets
+          .transfer(
+            1,
+            palletId,
+            api.createType("u128", (1400 * 1e12).toString()),
+            true
+          )
+          .signAndSend(walletSudo, async (result1) => {
+            console.log(`[TRANSFER] Current status is ${result1.status}`);
+            if (result1.status.isInBlock) {
+              console.log(
+                `[TRANSFER] Transaction included at blockHash ${result1.status.asInBlock}`
+              );
+            } else if (result1.status.isFinalized) {
+              console.log(
+                `[TRANSFER] Transaction finalized at blockHash ${result1.status.asFinalized}`
+              );
+            }
+          });
+      }
+    });
+
+  // const crPopRes = await crowdloanRewardsPopulateJSON(
+  //   api,
+  //   walletSudo,
+  //   [
+  //     { address: "5tfaf3MPRwzECcLhnzv75zvML1DHGJcvPYamoSNLoeAgGQ4S", rewards: "100"},
+  //     { address: "5y1hoDbgnAKsL9JZ3d1zkM5LYQQPaAnb9Un7VXMM2YipSeUE", rewards: "100"},
+  //     // peter dot wallets
+  //     { address: "5uymjr2xLL14upmg4nezH5LZMNgenGn7MrbQ2WnJ7dhcDb4C", rewards: "100"},
+  //     { address: "5z6opGwNemAtYG7o7KehBn2KdKbGPw64E23ZpwxcXoGiwufL", rewards: "100"},
+  //     { address: "E4syq7LfkjrZuqofYRg5dX2zzd8DR54p82F2BHuFLqHHGm6", rewards: "100"},
+  //     { address: "GRxsfLzj5wthacZ6bYSdL9FNosAMFBkVhcwWWxGtMsCSx8G", rewards: "100"},
+  //     { address: "FhJDi6usuBii4kbHEiUbYcd2a1yXk5CJCJEkxr2BT3wqHmc", rewards: "100"},
+  //     { address: "5GgjZECB6XsH3iao7rg6dDbMG9urjsWVjinDBF2ngqWFxyoC", rewards: "100"},
+  //     { address: "F53d3jeyFvb2eYsgAERhjC8mogao4Kg4GsdezrqiT8aj55v", rewards: "100"}, // liviu
+  //   ],
+  //   [
+  //     { address: myEth1.address, rewards: "100"},
+  //     { address: myEth2.address, rewards: "100"},
+  //     // peter eth wallets
+  //     { address: "0x33Cd07B1ae8485a6090091ee55389237eCB0Aed4", rewards: "100"},
+  //     { address: "0xfe302f2D69cAf32d71812587ECcd4fcDF8287E22", rewards: "100"},
+  //     { address: "0x38650E1FD89E6bBEfDD2f150190C70da02454b93", rewards: "100"},
+  //   ]
+  // );
+  // console.log(crPopRes.data.toHuman());
+  
   // const initRes = await initialize(api, walletSudo);
-
-  console.log(crPopRes.data.toHuman());
   // console.log(initRes.data.toHuman());
 };
 
