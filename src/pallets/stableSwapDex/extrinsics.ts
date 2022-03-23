97aac428b5fdd6d8ead669edee51f1cf69e215d2
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-03-23 17:49:12 +0500

    f types

--- src/pallets/stableSwapDex/extrinsics.ts
+++ src/pallets/stableSwapDex/extrinsics.ts
@@ -1,8 +1,5 @@
 import { KeyringPair } from "@polkadot/keyring/types";
-import { sendAndWaitForSuccess } from "polkadot-utils";
 import { ApiPromise } from "@polkadot/api";
-import { base58 } from "micro-base";
-import BigNumber from "bignumber.js";
 
 export const createLiquidityPool = async (
   api: ApiPromise,
