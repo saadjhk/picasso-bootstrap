b688f2fdf15c6e9d97f4f998c3f469b22e8197ca
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-04-13 15:57:58 +0500

    ch

--- src/index.ts
+++ src/index.ts
@@ -76,14 +76,6 @@ const main = async () => {
   )
   const api = await buildApi(process.env.PICASSO_RPC_URL || '', types, rpc)
 
-  // crowdloan rewards
-  // const walletBob = kr.addFromUri("//Bob");
-  // const crPopRes = await crowdloanRewardsPopulateTest(
-  //   api,
-  //   walletSudo,
-  //   [
-  //     myDot1,
-  //     myDot2,
   //     // peter dot wallets
   //     "5uymjr2xLL14upmg4nezH5LZMNgenGn7MrbQ2WnJ7dhcDb4C",
   //     "5z6opGwNemAtYG7o7KehBn2KdKbGPw64E23ZpwxcXoGiwufL",
@@ -92,115 +84,79 @@ const main = async () => {
   //     "FhJDi6usuBii4kbHEiUbYcd2a1yXk5CJCJEkxr2BT3wqHmc",
   //     "5GgjZECB6XsH3iao7rg6dDbMG9urjsWVjinDBF2ngqWFxyoC",
   //     "F53d3jeyFvb2eYsgAERhjC8mogao4Kg4GsdezrqiT8aj55v" // liviu
-  //   ] as any[],
-  //   [
-  //     myEth1.address,
-  //     myEth2.address,
   //     // peter eth wallets
   //     "0x33Cd07B1ae8485a6090091ee55389237eCB0Aed4",
   //     "0xfe302f2D69cAf32d71812587ECcd4fcDF8287E22",
   //     "0x38650E1FD89E6bBEfDD2f150190C70da02454b93",
-  //   ]
-  // );
-  // const ksm = Object.keys(rewards)
-  //   .filter((addr: string) => {
-  //     return !addr.startsWith("0x");
-  //   })
-  //   .map((addr) => {
-  //     return {
-  //       address: addr,
-  //       rewards: (rewards as any)[addr],
-  //     };
-  //   });
-  // const eth = Object.keys(rewards)
-  //   .filter((addr: string) => {
-  //     return addr.startsWith("0x");
-  //   })
-  //   .map((addr) => {
-  //     return {
-  //       address: addr,
-  //       rewards: (rewards as any)[addr],
-  //     };
-  //   });
-  // ksm.push({
-  //   address: "5tfaf3MPRwzECcLhnzv75zvML1DHGJcvPYamoSNLoeAgGQ4S",
-  //   rewards: "1223.231",
-  // });
-  // eth.push({
-  //   address: myEth1.address.toLowerCase(),
-  //   rewards: "1223.231",
-  // });
-  // const crPopRes = await crowdloanRewardsPopulateJSON(api, walletSudo, ksm.slice(0, 1500), eth.slice(0,500));
-  // let netRewards = Object.values(rewardsDev).reduce((acc, c) => {
-  //   let bnAcc = new BigNumber(acc);
-  //   bnAcc = bnAcc.plus(new BigNumber(c));
-  //   return bnAcc.toString();
-  // }, "0");
 
-  // let decimals = new BigNumber(10).pow(12);
-  // let base = new BigNumber(netRewards).times(decimals);
-
-  // const palletId = "5w3oyasYQg6vkbxZKeMG8Dz2evBw1P7Xr7xhVwk4qwwFkm8u";
-  // let mintResponse = await sendAndWaitForSuccess(
-  //   api,
-  //   walletSudo,
-  //   api.events.sudo.Sudid.is,
-  //   api.tx.sudo.sudo(api.tx.assets.mintInto(1, walletSudo.publicKey, api.createType("u128", base.toString())))
-  // );
-  // console.log(mintResponse.data.toHuman())
-  // let transferResponse = await sendAndWaitFor(
-  //   api,
-  //   walletSudo,
-  //   api.events.balances.Transfer.is,
-  //   api.tx.assets.transfer(
-  //     1,
-  //     palletId,
-  //     api.createType("u128", base.toString()),
-  //     true
-  //   )
-  // )
-  // console.log(transferResponse.data.toHuman())
-  // const crPopRes = await crowdloanRewardsPopulateJSON(
-  //   api,
-  //   walletSudo,
-  //   Object.keys(rewardsDev).filter(addr => !addr.startsWith("0x")).map((rewardAccount: string) => {
-  //     return {
-  //       address: rewardAccount,
-  //       rewards: (rewardsDev as any)[rewardAccount]
-  //     }
-  //   }),
-  //   Object.keys(rewardsDev).filter(addr => addr.startsWith("0x")).map((rewardAccount: string) => {
-  //     return {
-  //       address: rewardAccount,
-  //       rewards: (rewardsDev as any)[rewardAccount]
-  //     }
-  //   })
-  // );
-  // console.log(crPopRes.data.toHuman());
-  // const initRes = await initialize(api, walletSudo);
-  // console.log(initRes.data.toHuman());
-
-  const baseAmount = 250000000000;
-  const quoteAmount = 250000000000;
-  const baseAssetId = 4; // KUSAMA
-  const quoteAssetId = 129; // kUSD
-  const ownerFee = 10000;
-
-  await mintAssetsToWallet(api, walletSudo, walletSudo, [1, quoteAssetId, baseAssetId])
-  const end = api.createType('u32', api.consts.liquidityBootstrapping.maxSaleDuration);
-
-  const { data: [result] } = await createPool(
+  let netRewards = Object.values(rewardsDev).reduce((acc, c) => {
+    let bnAcc = new BigNumber(acc);
+    bnAcc = bnAcc.plus(new BigNumber(c));
+    return bnAcc.toString();
+  }, "0");
+  let decimals = new BigNumber(10).pow(12);
+  let base = new BigNumber(netRewards).times(decimals);
+  const palletId = "5w3oyasYQg6vkbxZKeMG8Dz2evBw1P7Xr7xhVwk4qwwFkm8u";
+  let mintResponse = await sendAndWaitForSuccess(
     api,
     walletSudo,
-    baseAssetId,
-    quoteAssetId,
-    ownerFee,
-    end
+    api.events.sudo.Sudid.is,
+    api.tx.sudo.sudo(api.tx.assets.mintInto(1, walletSudo.publicKey, api.createType("u128", base.toString())))
   );
+  console.log(mintResponse.data.toHuman())
+  let transferResponse = await sendAndWaitFor(
+    api,
+    walletSudo,
+    api.events.balances.Transfer.is,
+    api.tx.assets.transfer(
+      1,
+      palletId,
+      api.createType("u128", base.toString()),
+      true
+    )
+  )
+  console.log(transferResponse.data.toHuman())
+  const crPopRes = await crowdloanRewardsPopulateJSON(
+    api,
+    walletSudo,
+    Object.keys(rewardsDev).filter(addr => !addr.startsWith("0x")).map((rewardAccount: string) => {
+      return {
+        address: rewardAccount,
+        rewards: (rewardsDev as any)[rewardAccount]
+      }
+    }),
+    Object.keys(rewardsDev).filter(addr => addr.startsWith("0x")).map((rewardAccount: string) => {
+      return {
+        address: rewardAccount,
+        rewards: (rewardsDev as any)[rewardAccount]
+      }
+    })
+  );
+  console.log(crPopRes.data.toHuman());
+  const initRes = await initialize(api, walletSudo);
+  console.log(initRes.data.toHuman());
+  process.exit(0);
+  // const baseAmount = 250000000000;
+  // const quoteAmount = 250000000000;
+  // const baseAssetId = 4; // KUSAMA
+  // const quoteAssetId = 129; // kUSD
+  // const ownerFee = 10000;
 
-  console.log(result)
-  const liq = await addFundstoThePool(api, walletSudo, 0, baseAmount, quoteAmount)
-  console.log(liq)
+  // await mintAssetsToWallet(api, walletSudo, walletSudo, [1, quoteAssetId, baseAssetId])
+  // const end = api.createType('u32', api.consts.liquidityBootstrapping.maxSaleDuration);
+
+  // const { data: [result] } = await createPool(
+  //   api,
+  //   walletSudo,
+  //   baseAssetId,
+  //   quoteAssetId,
+  //   ownerFee,
+  //   end
+  // );
+
+  // console.log(result)
+  // const liq = await addFundstoThePool(api, walletSudo, 0, baseAmount, quoteAmount)
+  // console.log(liq)
 }
 
 cryptoWaitReady().then(() => {
