4aea0247dcd63901aede4ef70866548c0b2d8bfe
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-06-04 15:02:37 +0500

    Update tests

--- tests/tvlChart.test.ts
+++ tests/tvlChart.test.ts
@@ -19,4 +19,19 @@ describe('TVL Chart Series', () => {
     expect(series1w.length).toBe(1)
     expect(series1m.length).toBe(1)
   });
+  test('Test with no data', () => {
+    const subsquidData: [number, number][] = [
+    ];
+
+    let range: ChartRange = "24h";
+    const series24h = processTvlChartSeries(subsquidData, range);
+    range = "1w";
+    const series1w = processTvlChartSeries(subsquidData, range);
+    range = "1m";
+    const series1m = processTvlChartSeries(subsquidData, range);
+
+    expect(series24h.length).toBe(0)
+    expect(series1w.length).toBe(0)
+    expect(series1m.length).toBe(0)
+  });
 });
