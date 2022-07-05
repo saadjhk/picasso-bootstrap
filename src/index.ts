d432e9956cc30063c01890ecdc716184e3d9a17f
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-07-06 04:47:10 +0500

    chainName

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,13 @@ import { getSudoWallet, getSubstrateWallets, createRPC, createTypes, buildApi } 
 
 const main = async () => {
   const rpcUrl = process.env.RPC_URL || "ws://127.0.0.1:9988";
-  const sudoWallet = getSudoWallet(process.env.CHAIN_NAME || "dali-local");
+  const chainName = process.env.CHAIN_NAME || "dali-local";
+  
+  const sudoWallet = getSudoWallet(chainName);
   const dotWallets = getSubstrateWallets();
 
   const rpc = createRPC(definitions);
   const types = createTypes(definitions);
-
   const api = await buildApi(rpcUrl, types, rpc);
 
   await api.disconnect();
